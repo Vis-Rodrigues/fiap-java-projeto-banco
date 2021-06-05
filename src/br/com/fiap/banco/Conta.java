54ec3b390c3c16061527395b3cacac7e54e91e99
Administrator <servicoinmetrics734472@userede.com.br>
2021-06-04 23:00:14 -0300

    Alterando a classe Conta para o tipo bean e atualização javadoc

--- src/br/com/fiap/banco/Conta.java
+++ src/br/com/fiap/banco/Conta.java
@@ -2,26 +2,59 @@ package br.com.fiap.banco;
 
 /**
  * Classe que abstrai uma Conta Bancária
+ * 
  * @author inmetrics
  * @version 1.0
  */
 public class Conta {
-	
+
 	/**
 	 * Número da COnta
 	 */
-	public int numero;
+	private int numero;
 	/**
 	 * Número da Agencia
 	 */
-	public int agencia;
+	private int agencia;
 	/**
 	 * Saldo da conta
 	 */
-	public double saldo;
-	public Cliente cliente;
-	
+	private double saldo;
+	/**
+	 * Cliente dono da conta
+	 * @see Cliente
+	 */
+	private Cliente cliente;
+
 	public Conta() {
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 
 	public Conta(int numero, int agencia, double saldo) {
@@ -29,43 +62,39 @@ public class Conta {
 		this.agencia = agencia;
 		this.saldo = saldo;
 	}
-	
+
 	public Conta(int agencia) {
 		this.agencia = agencia;
 	}
-	
+
 	public Conta(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	public Conta(int agencia, double saldo) {
 		// Usando o construtor da linha 13
 		this(saldo);
 		this.agencia = agencia;
 	}
-	
+
 	/**
 	 * Depositar um valor ao saldo da conta
-	 * @param valor Valor a ser depositado
+	 * 
+	 * @param valor
+	 *            Valor a ser depositado
 	 */
 	public void depositar(double valor) {
 		this.saldo += valor;
 	}
-	
+
 	/**
 	 * Retira um valor do saldo da conta
-	 * @param valor Valor a ser retirado
+	 * 
+	 * @param valor
+	 *            Valor a ser retirado
 	 */
 	public void retirar(double valor) {
 		this.saldo -= valor;
 	}
-	
-	/**
-	 * Verifica o saldo da conta
-	 * @return Valor do saldo da conta
-	 */
-	public double verificarSaldo() {
-		return this.saldo;
-	}
-	
+
 }
--- src/br/com/fiap/banco/teste/Teste.java
+++ src/br/com/fiap/banco/teste/Teste.java
@@ -6,24 +6,24 @@ public class Teste {
 
 	public static void main(String[] args) {
 		Conta cc = new Conta();
-		cc.saldo = 50;
-		cc.agencia = 123;
-		cc.numero = 321;
+		cc.depositar(50);
+		cc.setAgencia(123);
+		cc.setNumero(321);
 		
 		cc.depositar(100);
 		if(cc!=null) {
-			System.out.println(String.format("Saldo disponível na conta: %s", cc.verificarSaldo()));
+			System.out.println(String.format("Saldo disponível na conta: %s", cc.getSaldo()));
 		}
 		
 		Conta poupanca = new Conta(111,222,1000);
 		poupanca.retirar(50);
-		System.out.println(String.format("Saldo disponível na poupança: %s", poupanca  .verificarSaldo()));
+		System.out.println(String.format("Saldo disponível na poupança: %s", poupanca.getSaldo()));
 		
 		// Foram criados dois construtores na classe, um recebe double e o outro int, o
 		// q vai fazer com q o Java reconheça, que é o construtor double q queremos é o
 		// ponto flutuante
 		Conta consDouble = new Conta(50.0);
-		System.out.println(String.format("Agência: %s e Saldo: %s", consDouble.agencia, consDouble.saldo));
+		System.out.println(String.format("Agência: %s e Saldo: %s", consDouble.getAgencia(), consDouble.getSaldo()));
 		
 	}
 
